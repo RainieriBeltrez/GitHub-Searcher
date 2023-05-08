597e7d65374f979e9406b022b50d1988aef07b36
RainieriBeltrez <rainierisbeltrez@gmail.com>
2023-05-07 20:49:45 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+const App = () => {
+  return(
+    <Container sx={{
+      background: 'whitesmoke',
+      width: '80vw',
+      height: '500px',
+      borderRadius: '16px',
+      marginTop: '40px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }} >
+      <Searcher />
+    </Container>
+    
+  )
+};
+
+export default App;
