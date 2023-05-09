95b93992ca5d1aa2920b539b136d72dc27257c57
RainieriBeltrez <rainierisbeltrez@gmail.com>
2023-05-09 00:27:01 -0400

    searcher changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Container } from "@mui/material";
 
+import Searcher from "./Components/Searcher";
+
+
 const App = () => {
   return(
     <Container sx={{
-      background: 'whitesmoke',
+      background: '#100e17',
       width: '80vw',
       height: '500px',
       borderRadius: '16px',
       marginTop: '40px',
       display: 'flex',
       flexDirection: 'column',
-      alignItems: 'center'
+      alignItems: 'center',
+      boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+      overflow: "hidden",
     }} >
       <Searcher />
     </Container>
--- src/Components/Searcher/index.jsx
+++ src/Components/Searcher/index.jsx
@@ -1,11 +1,50 @@
 import React from "react";
-import { IconButton, Stack, TextField } from "@mui/material";
+import { IconButton, Stack, InputAdornment, TextField, InputLabel } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 const Searcher = () => {
     return(
-        <Stack>
-            <TextField></TextField>
-            <IconButton></IconButton>
+        <Stack 
+        direction="row"
+        sx={{
+            marginTop: '30px',
+            width: '80%'
+        }
+        }>
+            <TextField 
+            id="outlined-basic"
+            variant="outlined"
+            placeholder="Search" 
+            size="small"
+            InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton sx={{
+                        background: '#100e17',
+                        
+                        height: '36px',
+                        right: '-10px'
+                    }}>
+                      <SearchIcon 
+                      sx={{
+                        color: 'white'
+                      }}/>
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            InputLabelProps={{
+                shrink: false
+              }}
+            sx={{ 
+                [`& fieldset`]:{
+                    borderRadius: '50px',
+                },
+                width: '90%',
+                backgroundColor: 'white',
+                borderRadius: '50px'
+                }}/>
+            
         </Stack>
     )
 }
